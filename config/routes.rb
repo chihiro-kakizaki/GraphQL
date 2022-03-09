67d444cf687e9efbd11b031298b2f6cc7971ac81
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-09 11:47:55 +0900

    add:graphql実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "blogs#index"
   resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  post "/graphql", to: "graphql#execute"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
 end
